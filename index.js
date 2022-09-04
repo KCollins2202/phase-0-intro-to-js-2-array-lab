3f95361be2f9971d488fbe40dabb647a5efc6d70
Collins Kimani Karanja <kimcolinss@gmail.com>
2022-09-04 15:10:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+function appendCat(name){
+    var allCats = [...cats,"Broom"];
+    return allCats;
+    
+}
+function prependCat(name){
+    const wildCats = ["Arnold",...cats];
+    return wildCats;
+}
+function removeLastCat(name){
+    const cats =["Milo","Otis","Garfield"];
+    cats.pop();
+    return cats;
+}
+function removeFirstCat(name){
+    const cats = ["Milo","Otis","Garfield"];
+    cats.shift();
+    return cats;
+}
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
